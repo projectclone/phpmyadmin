954a0520ee729302d3b1d623ce9c44d1f881e734
root <root@ctrl-vm-u.madadm.com>
2018-02-11 05:43:49 -0500

    update config

--- config.inc.php
+++ config.inc.php
@@ -29,7 +29,7 @@ $i++;
 /* Authentication type */
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
 /* Server parameters */
-$cfg['Servers'][$i]['host'] = 'localhost';
+$cfg['Servers'][$i]['host'] = '127.0.0.1';
 $cfg['Servers'][$i]['compress'] = false;
 $cfg['Servers'][$i]['AllowNoPassword'] = true;
 
